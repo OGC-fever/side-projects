9a626cb0c56b906fa9df65c9288a82d809d71ee8
OGC-fever <seal.heart@gmail.com>
2020-03-28 01:40:41 +0800

    1

--- criminal map/criminal map.py
+++ criminal map/criminal map.py
@@ -6,4 +6,14 @@ import numpy as np
 url = 'https://data.gov.tw/dataset/14200'
 req = requests.get(url)
 soup = bs4.BeautifulSoup(req.content, 'html.parser')
-soup.find_all(class_ = 'dgresource')[0]
+
+soup1 = soup.find_all(class_ = 'dgresource', title="下載格式為 CSV")
+soup2 = soup.find_all(class_ = 'ff-desc')
+
+for i in range(len(soup2)):
+    print(soup2[i].text)
+
+for i in range(len(soup1)):
+    print(soup1[i].text)
+
+
