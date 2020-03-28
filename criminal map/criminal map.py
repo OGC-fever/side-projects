ac9a8d385544f3262922633413afdec013a7ac70
OGC-fever <seal.heart@gmail.com>
2020-03-29 00:03:02 +0800

    hha

--- criminal map/criminal map.py
+++ criminal map/criminal map.py
@@ -7,11 +7,12 @@ url = 'https://data.gov.tw/dataset/14200'
 req = requests.get(url)
 soup = bs4.BeautifulSoup(req.content, 'html.parser')
 titles = soup.find_all('span', class_ = 'ff-desc')
-links = soup.find_all('a', class_ = 'dgresource', title="下載格式為 API")
+links = soup.find_all('a', class_ = 'dgresource', title = "下載格式為 CSV")
 
 table_array = []
 for i, j in zip(range(0,len(titles),2), range(len(links))):
     table_array.append([titles[i].text, links[j].text])
+
 table_array = np.array(table_array)
 data = pd.DataFrame(table_array)
 data.sort_values(by = data.columns[0], inplace = True)
@@ -20,5 +21,11 @@ data.reset_index(drop = 1, inplace = True)
 for i in range(len(data)):
     print(str(i + 1) + "." + data[0][i])
 
-data_index = int(input('which data do you want to show ? '))
-print('you select: ' + str(data_index) + "." + data[0][data_index - 1])
+data_index = int(input('which data do you want to show ? ')) - 1
+20
+print('you select: ' + str(data_index + 1) + "." + data[0][data_index])
+url = data[1][data_index]
+criminal_data = pd.read_csv(url)
+county_criminal = criminal_data[criminal_data[criminal_data.columns[-1]].str.contains('彰化', na = False)]
+county_criminal.rename(columns = {str(county_criminal.columns[1]):'date' ,str(county_criminal.columns[2]):'location'}, inplace = True)
+print(county_criminal)
