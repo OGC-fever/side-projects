37c8b20816a1bafa0af2d9e034dc3b1cb3277401
OGC-fever <seal.heart@gmail.com>
2020-03-28 21:21:23 +0800

    fun

--- criminal map/criminal map.py
+++ criminal map/criminal map.py
@@ -7,10 +7,10 @@ url = 'https://data.gov.tw/dataset/14200'
 req = requests.get(url)
 soup = bs4.BeautifulSoup(req.content, 'html.parser')
 
-soup1 = soup.find_all(class_ = 'dgresource', title="下載格式為 CSV")
-soup2 = soup.find_all(class_ = 'ff-desc')
+soup1 = soup.find_all('a', class_ = 'dgresource', title="下載格式為 API")
+soup2 = soup.find_all('span', class_ = 'ff-desc')
 
-for i in range(len(soup2)):
+for i in range(0,len(soup2),2):
     print(soup2[i].text)
 
 for i in range(len(soup1)):
