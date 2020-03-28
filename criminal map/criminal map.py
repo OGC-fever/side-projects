602de68b820b99ccf492058e4014c3863e5e205c
OGC-fever <seal.heart@gmail.com>
2020-03-29 02:42:46 +0800

    test

--- criminal map/criminal map.py
+++ criminal map/criminal map.py
@@ -2,30 +2,30 @@ import pandas as pd
 import bs4
 import requests
 import numpy as np
-
+from bs4 import BeautifulSoup as bs
+# get data
 url = 'https://data.gov.tw/dataset/14200'
-req = requests.get(url)
-soup = bs4.BeautifulSoup(req.content, 'html.parser')
+soup = bs(requests.get(url).content, 'html.parser')
 titles = soup.find_all('span', class_ = 'ff-desc')
-links = soup.find_all('a', class_ = 'dgresource', title = "下載格式為 CSV")
-
+links = soup.find_all('a', class_ = 'dgresource', title = "下載格式為 API")
+# convert data to array
 table_array = []
 for i, j in zip(range(0,len(titles),2), range(len(links))):
     table_array.append([titles[i].text, links[j].text])
-
-table_array = np.array(table_array)
+# convert array to pandas
 data = pd.DataFrame(table_array)
 data.sort_values(by = data.columns[0], inplace = True)
 data.reset_index(drop = 1, inplace = True)
-
+# print gotten data
 for i in range(len(data)):
     print(str(i + 1) + "." + data[0][i])
-
+# show select data and read into pandas
 data_index = int(input('which data do you want to show ? ')) - 1
-20
 print('you select: ' + str(data_index + 1) + "." + data[0][data_index])
 url = data[1][data_index]
-criminal_data = pd.read_csv(url)
-county_criminal = criminal_data[criminal_data[criminal_data.columns[-1]].str.contains('彰化', na = False)]
+select_soup = bs(requests.get(url).content, 'html.parser')
+temp = pd.read_json(str(select_soup))
+season_criminal = pd.DataFrame(temp[temp.columns[1]][2])
+county_criminal = season_criminal[season_criminal[season_criminal.columns[-1]].str.contains('彰化', na = False)]
 county_criminal.rename(columns = {str(county_criminal.columns[1]):'date' ,str(county_criminal.columns[2]):'location'}, inplace = True)
 print(county_criminal)
