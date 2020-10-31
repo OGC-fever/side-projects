396ed80761fd379e92e5a1a5acac45364812279c
OGC-fever <seal.heart@gmail.com>
2020-11-01 00:56:00 +0800

    test

--- imgur new/show.py
+++ imgur new/show.py
@@ -1,5 +1,30 @@
-# from packages.showdata import *
-from packages.getdata import *
+import random
+from random import randint
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+
+url = "https://imgur.com/hot/time"
+page = requests.get(url)
+soup = bs(page.content, "lxml")
+output = soup.find_all("a", {"class": "image-list-link", "data-page": "0"})
+link_prefix = "https://imgur.com"
+link = []
+for i in range(len(output)):
+    link.append(link_prefix + output[i]['href'])
+img_columns = 3
+url = link[0:img_columns]
+page, soup, img_url, pic = [], [], [], []
+for i in range(len(url)):
+    page.append(requests.get(url[i]))
+    soup.append(bs(page[i].content, 'lxml'))
+    img_url.append(soup[i].find_all(
+        'meta', {'name': "twitter:image"})[0]["content"])
+    pic.append(np.array(Image.open(BytesIO(requests.get(img_url[i]).content))))
 
 img_hight = 5
 fig, ax = plt.subplots(1, len(url))
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # OGC-fever's Side Projects
 
-Personal side projects just for fun & practice
+Personal side projects just for fun & practice & memo
 
 ## hardness
 - visualize relationship between hardness of water and places in taiwan
 
 ## imgur new
 - fetch newest viral images on imgur
+
+## population ratio
+- visualize ratio curve of males vs. females
