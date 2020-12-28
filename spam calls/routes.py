9ecd0bf921369fe5414a0a2c55076304ccb09800
OGC-fever <seal.heart@gmail.com>
2020-12-29 01:37:08 +0800

    remove except

--- spam calls/routes.py
+++ spam calls/routes.py
@@ -19,44 +19,44 @@ def add():
 
 @app.route("/add_calls", methods=["POST"])
 def add_calls():
-    if request.method == 'POST':
-        phone = request.form['phone']
-        comments = request.form['comments']
-        sql = f"insert into calls (phone, comments) values (?, ?)"
-        try:
-            con = sqlite3.connect(database)
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute(sql, (phone, comments))
-            con.commit()
-            con.close()
-            msg = "Successfully"
-            data = [phone, comments]
-        except:
-            con.rollback()
-            return render_template("oops.html")
+    # if request.method == 'POST':
+    phone = request.form['phone']
+    comments = request.form['comments']
+    sql = "insert into calls (phone, comments) values (?, ?)"
+        # try:
+    con = sqlite3.connect(database)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(sql, (phone, comments))
+    con.commit()
+    con.close()
+    msg = "Successfully"
+    data = [phone, comments]
+        # except:
+        #     con.rollback()
+        #     return render_template("oops.html")
     return render_template("result.html", data=data, msg=msg, action='add')
 
 
 @app.route("/query_calls", methods=["POST"])
 def query_calls():
-    if request.method == 'POST':
-        phone = request.form['phone']
-        sql = f"select * from calls where phone = ?"
-        try:
-            con = sqlite3.connect(database)
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute(sql, (phone,))
-            data = cur.fetchall()
-            con.close()
-            msg = ""
-            if data == []:
-                msg = None
-                return render_template("result.html", action='query', msg=msg)
-        except:
-            con.rollback()
-            return render_template("oops.html")
+    # if request.method == 'POST':
+    phone = request.form['phone']
+    sql = "select * from calls where phone = ?"
+        # try:
+    con = sqlite3.connect(database)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(sql, (phone,))
+    data = cur.fetchall()
+    con.close()
+    msg = ""
+    if data == []:
+        msg = None
+        return render_template("result.html", action='query', msg=msg)
+        # except:
+        #     con.rollback()
+        #     return render_template("oops.html")
     return render_template("result.html", data=data, action='query', msg=msg)
 
 
@@ -65,61 +65,63 @@ def not_found(e):
     return render_template("oops.html")
 
 
-@app.route("/")
-@app.route("/about")
+@app.route("/", methods=["POST", "GET"])
+@app.route("/about", methods=["POST", "GET"])
 def about():
-    sql = f"select * from calls order by id desc limit 5"
-    if request.method == 'GET':
-        try:
-            con = sqlite3.connect(database)
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute(sql)
-            data = cur.fetchall()
-            con.close()
-            msg = ""
-            if data == []:
-                msg = None
-                return render_template("result.html", action='query', msg=msg)
-        except:
-            con.rollback()
-            return render_template("oops.html")
+    sql = "select * from calls order by id desc limit 5"
+    # if request.method == 'GET':
+    # try:
+    con = sqlite3.connect(database)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+    con.close()
+    msg = ""
+    if data == []:
+        msg = None
+        return render_template("result.html", action='query', msg=msg)
+    # except:
+    #     con.rollback()
+    #     return render_template("oops.html")
     return render_template("about.html", data=data)
 
 
-@app.route("/rank_up/<int:id>")
+@app.route("/rank_up/<int:id>", methods=["POST", "GET"])
 def rank_up(id):
-    sql = f"update calls set rank = rank + 1 where id = ?"
-    try:
-        con = sqlite3.connect(database)
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute(sql, (id,))
-        con.commit()
-        cur.execute(f"select * from calls where id = '{id}'")
-        data = cur.fetchall()
-        con.close()
-    except:
-        con.rollback()
-        return render_template("oops.html")
+    sql = "update calls set rank = rank + 1 where id = ?"
+    # try:
+    con = sqlite3.connect(database)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(sql, (id,))
+    con.commit()
+    sql = "select * from calls where id = ?"
+    cur.execute(sql, (id,))
+    data = cur.fetchall()
+    con.close()
+    # except:
+    #     con.rollback()
+    #     return render_template("oops.html")
     return render_template("result.html", data=data, action='query')
 
 
-@app.route("/rank_down/<int:id>")
+@app.route("/rank_down/<int:id>", methods=["POST", "GET"])
 def rank_down(id):
-    sql = f"update calls set rank = rank - 1 where id = ?"
-    try:
-        con = sqlite3.connect(database)
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute(sql, (id,))
-        con.commit()
-        cur.execute(f"select * from calls where id = '{id}'")
-        data = cur.fetchall()
-        con.close()
-    except:
-        con.rollback()
-        return render_template("oops.html")
+    sql = "update calls set rank = rank - 1 where id = ?"
+    # try:
+    con = sqlite3.connect(database)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(sql, (id,))
+    con.commit()
+    sql = "select * from calls where id = ?"
+    cur.execute(sql, (id,))
+    data = cur.fetchall()
+    con.close()
+    # except:
+    #     con.rollback()
+    #     return render_template("oops.html")
     return render_template("result.html", data=data, action='query')
 
 
