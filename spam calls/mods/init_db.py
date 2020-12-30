29c7d59d56044b395f6f676ed9bb78c245216454
OGC-fever <seal.heart@gmail.com>
2020-12-30 23:33:08 +0800

    update

--- spam calls/mods/init_db.py
+++ spam calls/mods/init_db.py
@@ -1,19 +1,15 @@
 import sqlite3
 
 
-def init_db():
-    con = sqlite3.connect(database)
-    cur = con.cursor()
-    cur.executescript('''
+def init_db(database):
+    with sqlite3.connect(database) as con:
+        cur = con.cursor()
+        cur.executescript('''
         create table if not exists calls (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             phone TEXT NOT NULL,
             comments TEXT NOT NULL,
             time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             rank INTEGER DEFAULT 0
-        );''')
-    con.commit()
-    con.close()
-
-
-database = 'spam_calls.db'
+            );''')
+        con.commit()
--- spam calls/routes.py
+++ spam calls/routes.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
-from db_crud import init_db
-from mods.about import about
+from mods.init_db import init_db
 import sqlite3
 from werkzeug.exceptions import HTTPException, InternalServerError
 
 app = Flask(__name__)
-init_db()
 database = 'spam_calls.db'
+init_db(database)
 
 
 @app.route("/query")
@@ -21,44 +20,40 @@ def add():
 
 @app.route("/add_calls", methods=["POST"])
 def add_calls():
-    # if request.method == 'POST':
     phone = request.form['phone']
     comments = request.form['comments']
     sql = "insert into calls (phone, comments) values (?, ?)"
-    # try:
-    con = sqlite3.connect(database)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute(sql, (phone, comments))
-    con.commit()
-    con.close()
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(sql, (phone, comments))
+        con.commit()
     msg = "Successfully"
     data = {"phone": phone, "comments": comments}
-    # except:
-    #     con.rollback()
-    #     return render_template("oops.html")
     return render_template("result.html", data=data, msg=msg, action='add')
 
 
 @app.route("/query_calls", methods=["POST"])
 def query_calls():
-    # if request.method == 'POST':
     phone = request.form['phone']
-    sql = "select * from calls where phone like ? order by time desc"
-    # try:
-    con = sqlite3.connect(database)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute(sql, ('%'+phone+'%',))
-    data = cur.fetchall()
-    con.close()
+    rank = request.form['rank']
+    # return phone
+    if rank == "":
+        sql = "select * from calls where phone like ? order by time desc"
+    else:
+        sql = "select * from calls where phone like ? and rank >= ? order by time desc"
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        if rank == "":
+            cur.execute(sql, ('%'+phone+'%',))
+        else:
+            cur.execute(sql, ('%'+phone+'%', rank))
+        data = cur.fetchall()
     msg = ""
     if data == []:
         msg = None
         return render_template("result.html", action='query', msg=msg)
-        # except:
-        #     con.rollback()
-        #     return render_template("oops.html")
     return render_template("result.html", data=data, action='query', msg=msg)
 
 
@@ -70,47 +65,48 @@ def not_found(e):
 
 @app.route("/", methods=["POST", "GET"])
 @app.route("/about", methods=["POST", "GET"])
-def index():
-    return about(database)
+def about():
+    sql = "select * from calls order by id desc limit 5"
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(sql)
+        data = cur.fetchall()
+    msg = ""
+    if data == []:
+        msg = None
+        return render_template("result.html", action='query', msg=msg)
+    return render_template("about.html", data=data)
 
 
 @app.route("/rank_up/<int:id>", methods=["POST", "GET"])
 def rank_up(id):
-    sql = "update calls set rank = rank + 1 where id = ?"
-    # try:
-    con = sqlite3.connect(database)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute(sql, (id,))
-    con.commit()
-    sql = "select * from calls where id = ?"
-    cur.execute(sql, (id,))
-    data = cur.fetchall()
-    con.close()
-    # except:
-    #     con.rollback()
-    #     return render_template("oops.html")
+    sql = {"update": "update calls set rank = rank + 1 where id = ?",
+           "read": "select * from calls where id = ?"}
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(sql["update"], (id,))
+        con.commit()
+        cur.execute(sql["read"], (id,))
+        data = cur.fetchall()
     return render_template("result.html", data=data, action='query')
 
 
 @app.route("/rank_down/<int:id>", methods=["POST", "GET"])
 def rank_down(id):
-    sql = "update calls set rank = rank - 1 where id = ?"
-    # try:
-    con = sqlite3.connect(database)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute(sql, (id,))
-    con.commit()
-    sql = "select * from calls where id = ?"
-    cur.execute(sql, (id,))
-    data = cur.fetchall()
-    con.close()
-    # except:
-    #     con.rollback()
-    #     return render_template("oops.html")
+    sql = {"update": "update calls set rank = rank - 1 where id = ?",
+           "read": "select * from calls where id = ?"}
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(sql["update"], (id,))
+        con.commit()
+        cur.execute(sql["read"], (id,))
+        data = cur.fetchall()
     return render_template("result.html", data=data, action='query')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run()
