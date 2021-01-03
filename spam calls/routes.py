e37293625a4c4bcc6b05b62e4705aef16b1fab6a
OGC-fever <seal.heart@gmail.com>
2021-01-03 18:58:15 +0800

    refactory

--- spam calls/routes.py
+++ spam calls/routes.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from mods.init_db import init_db
+from flask import Flask, render_template, request, url_for, redirect
+from init_db import init_db
 import sqlite3
 from werkzeug.exceptions import HTTPException, InternalServerError
 
@@ -8,23 +8,47 @@ database = 'spam_calls.db'
 init_db(database)
 
 
-@app.route("/message")
-def bbs():
-    return render_template("message.html")
+@app.route("/", methods=["GET"])
+@app.route("/about", methods=["GET"])
+def about():
+    sql = "select * from calls order by id desc limit 5"
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(sql)
+        data = cur.fetchall()
+    if data == []:
+        return render_template("result.html", action='query', msg="")
+    return render_template("about.html", data=data)
 
 
-@app.route("/query")
-def query():
-    return render_template("query_calls.html")
+@app.route("/msg", methods=["GET", "POST"])
+def msg():
+    if request.method == "GET":
+        sql = "select * from msgs order by id desc limit 20"
+        with sqlite3.connect(database) as con:
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute(sql)
+            data = cur.fetchall()
+        if data == []:
+            return render_template("message.html", data="")
+        return render_template("message.html", data=data)
+    name = request.form['name']
+    msg = request.form['msg']
+    sql = "insert into msgs (name, msg) values (?, ?)"
+    with sqlite3.connect(database) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(sql, (name, msg))
+        con.commit()
+    return redirect(url_for("msg"))
 
 
-@app.route("/add")
+@app.route("/add", methods=["POST", "GET"])
 def add():
-    return render_template("add_calls.html")
-
-
-@app.route("/add_calls", methods=["POST"])
-def add_calls():
+    if request.method == "GET":
+        return render_template("add.html")
     phone = request.form['phone']
     comments = request.form['comments']
     sql = "insert into calls (phone, comments) values (?, ?)"
@@ -38,27 +62,26 @@ def add_calls():
     return render_template("result.html", data=data, msg=msg, action='add')
 
 
-@app.route("/query_calls", methods=["POST"])
-def query_calls():
+@app.route("/query", methods=["POST", "GET"])
+def query():
+    if request.method == "GET":
+        return render_template("query.html")
     phone = request.form['phone']
     rank = request.form['rank']
-    if rank == "":
-        sql = "select * from calls where phone like ? order by time desc"
-    else:
-        sql = "select * from calls where phone like ? and rank >= ? order by time desc"
+    sql = {"rank_empty": "select * from calls where phone like ? order by time desc limit 10",
+           "rank": "select * from calls where phone like ? and rank >= ? order by time desc limit 10"}
     with sqlite3.connect(database) as con:
         con.row_factory = sqlite3.Row
         cur = con.cursor()
         if rank == "":
-            cur.execute(sql, ('%'+phone+'%',))
+            cur.execute(sql["rank_empty"], ('%'+phone+'%',))
         else:
-            cur.execute(sql, ('%'+phone+'%', rank))
+            cur.execute(sql["rank"], ('%'+phone+'%', rank))
         data = cur.fetchall()
-    msg = ""
     if data == []:
-        msg = None
+        msg = "No record"
         return render_template("result.html", action='query', msg=msg)
-    return render_template("result.html", data=data, action='query', msg=msg)
+    return render_template("result.html", data=data, action='query')
 
 
 @app.errorhandler(HTTPException)
@@ -67,23 +90,7 @@ def not_found(e):
     return render_template("oops.html")
 
 
-@app.route("/", methods=["POST", "GET"])
-@app.route("/about", methods=["POST", "GET"])
-def about():
-    sql = "select * from calls order by id desc limit 5"
-    with sqlite3.connect(database) as con:
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute(sql)
-        data = cur.fetchall()
-    msg = ""
-    if data == []:
-        msg = None
-        return render_template("result.html", action='query', msg=msg)
-    return render_template("about.html", data=data)
-
-
-@app.route("/rank_up/<int:id>", methods=["POST", "GET"])
+@app.route("/rank_up/<int:id>", methods=["GET"])
 def rank_up(id):
     sql = {"update": "update calls set rank = rank + 1 where id = ?",
            "read": "select * from calls where id = ?"}
@@ -97,7 +104,7 @@ def rank_up(id):
     return render_template("result.html", data=data, action='query')
 
 
-@app.route("/rank_down/<int:id>", methods=["POST", "GET"])
+@app.route("/rank_down/<int:id>", methods=["GET"])
 def rank_down(id):
     sql = {"update": "update calls set rank = rank - 1 where id = ?",
            "read": "select * from calls where id = ?"}
