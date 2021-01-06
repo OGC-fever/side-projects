22a9f2fa3fa9a59912c43592e3cf1f22b2565785
OGC-fever <seal.heart@gmail.com>
2021-01-06 22:25:12 +0800

    base64 image

--- 99 msg/init_db.py
+++ 99 msg/init_db.py
@@ -1,10 +1,12 @@
 import sqlite3
 
 sql = {"init_msg": "create table if not exists msg (\
-                id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                name TEXT NOT NULL,\
-                msg TEXT NOT NULL,\
-                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"}
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,\
+                    name TEXT NOT NULL,\
+                    msg TEXT NOT NULL,\
+                    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
+                    image blob,\
+                    thumbnail blob)"}
 
 
 def init_db(database):
--- 99 msg/routes.py
+++ 99 msg/routes.py
@@ -1,40 +1,78 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask.helpers import flash
 from init_db import init_db
 import sqlite3
 import random
 from werkzeug.exceptions import HTTPException, InternalServerError
+import base64
+from io import BytesIO
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
+
 database = 'msg.db'
 init_db(database)
+
+
+# @app.route("/test", methods=["GET"])
+# def test():
+#     # img = plt.imread("static/pic/upload/test.png")
+#     # buf = BytesIO()
+#     # plt.imsave(buf, img, format="png")
+#     # data = base64.b64encode(buf.getbuffer()).decode("ascii")
+
+#     sql = "select image from msg order by id desc limit 1"
+#     with sqlite3.connect(database) as con:
+#         con.row_factory = sqlite3.Row
+#         cur = con.cursor()
+#         cur.execute(sql)
+#         data = cur.fetchone()[0]
+#     return f"<img src='data:image/png;base64,{data}'/>"
 
 
 @app.route("/about", methods=["GET"])
 def about():
     return render_template("about.html")
 
+
+def check_file(filename, extensions):
+    if "." in filename and filename.split(".")[-1].lower() in extensions:
+        return True
+    else:
+        return False
+
+
 @app.route("/", methods=["GET"])
 @app.route("/msg", methods=["GET", "POST"])
 def msg():
+    sql = {"read": "select * from msg order by id desc limit 99",
+           "create": "insert into msg (name, msg, image) values (?, ?, ?)"}
     if request.method == "GET":
-        sql = "select * from msg order by id desc limit 99"
         with sqlite3.connect(database) as con:
             con.row_factory = sqlite3.Row
             cur = con.cursor()
-            cur.execute(sql)
+            cur.execute(sql["read"])
             data = cur.fetchall()
         if data == []:
             return render_template("message.html", data="")
         return render_template("message.html", data=data)
     name = request.form['name']
-    msg = request.form['msg']
-    if name == "":
+    if not name:
         name = random.choice(["nobody", "anonymous", "路人甲", "無名"])
-    sql = "insert into msg (name, msg) values (?, ?)"
+    msg = request.form['msg']
+    file = request.files["upload"]
+    extensions = {'jpg', 'jpeg', "png", "gif"}
+    if file.filename and check_file(file.filename, extensions):
+        buf = BytesIO()
+        file.save(buf)
+        image = base64.b64encode(buf.getbuffer())
+    else:
+        image = None
     with sqlite3.connect(database) as con:
         con.row_factory = sqlite3.Row
         cur = con.cursor()
-        cur.execute(sql, (name, msg))
+        cur.execute(sql["create"], (name, msg, image))
         con.commit()
     return redirect(url_for("msg"))
 
