b6cfe6e0410d7f15aad17e4297a1b58b25672afe
OGC-fever <seal.heart@gmail.com>
2021-01-08 21:27:42 +0800

    pythonanywhere fix

--- 99 msg/routes.py
+++ 99 msg/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, send_file
+from flask import Flask, render_template, request, url_for, redirect, send_file, Response
 from flask.helpers import flash
 from PIL import Image
 from init_db import init_db
@@ -21,7 +21,7 @@ def about():
 
 
 def check_file(filename, file_exts):
-    if "." in filename and filename.split(".")[-1].lower() in file_exts:
+    if filename and "." in filename and filename.split(".")[-1].lower() in file_exts:
         return True
     else:
         return False
@@ -42,8 +42,10 @@ def image_route(id):
         cur = con.cursor()
         cur.execute(sql, (id,))
         result = cur.fetchone()
-        image = BytesIO(result[0])
-    return send_file(image, mimetype="image/png")
+    image = BytesIO(result[0])
+    # return send_file(image, mimetype="image/png")
+    # for pythonanywhere
+    return Response(image, mimetype='image/jpeg', direct_passthrough=True)
 
 
 @app.route("/", methods=["GET"])
@@ -63,24 +65,23 @@ def msg():
     name = request.form['name']
     msg = request.form['msg']
     file = request.files["upload"]
-    file_exts = {'jpg', 'jpeg', "png", "gif"}
-    thumbnail_size = (500, 500)
-    image, thumbnail = None, None
+    file_exts = {'jpg', 'jpeg', "jfif", "png", "gif"}
+    thumbnail_size = (400, 400)
     if not name:
         name = random.choice(["nobody", "anonymous", "路人甲", "無名"])
-    if not msg and not check_file(file.filename, file_exts):
-        return redirect(url_for("msg"))
-    if file.filename and check_file(file.filename, file_exts):
-        buf_file = BytesIO()
-        buf_thumb = BytesIO()
+    if check_file(file.filename, file_exts):
+        buf_file, buf_thumb = BytesIO(), BytesIO()
         file.save(buf_file)
         image = sqlite3.Binary(buf_file.getbuffer())
-        im = Image.open(file)
-        im.thumbnail(thumbnail_size)
-        im.save(buf_thumb, get_file_ext(file.filename))
+        im_thumb = Image.open(file)
+        im_thumb.thumbnail(thumbnail_size)
+        im_thumb.save(buf_thumb, get_file_ext(file.filename))
         thumbnail = base64.b64encode(buf_thumb.getbuffer()).decode()
     else:
-        return redirect(url_for("msg"))    
+        if msg:
+            image, thumbnail = None, None
+        else:
+            return redirect(url_for("msg"))
     with sqlite3.connect(database) as con:
         con.row_factory = sqlite3.Row
         cur = con.cursor()
