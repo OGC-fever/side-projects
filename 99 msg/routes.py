918569a57e10370798f60be55f348b32ed135153
OGC-fever <seal.heart@gmail.com>
2021-01-09 22:14:42 +0800

    refactory

--- 99 msg/routes.py
+++ 99 msg/routes.py
@@ -5,7 +5,6 @@ from init_db import init_db
 import sqlite3
 import random
 from werkzeug.exceptions import HTTPException, InternalServerError
-import base64
 from io import BytesIO
 
 
@@ -15,12 +14,8 @@ database = 'msg.db'
 init_db(database)
 
 
-@app.route("/about", methods=["GET"])
-def about():
-    return render_template("about.html")
-
-
-def check_file(filename, file_exts):
+def check_file(filename):
+    file_exts = {'jpg', 'jpeg', "jfif", "png", "gif"}
     if filename and "." in filename and filename.split(".")[-1].lower() in file_exts:
         return True
     else:
@@ -34,28 +29,31 @@ def get_file_ext(filename):
     return ext
 
 
-@app.route("/image/<int:id>", methods=["GET", "POST"])
-def image_route(id):
-    sql = "select image from msg where id = ?"
+def db_crud(database, sql, prm, fetchall, commit):
     with sqlite3.connect(database) as con:
         con.row_factory = sqlite3.Row
         cur = con.cursor()
-        cur.execute(sql, (id,))
-        result = cur.fetchone()
-    image = BytesIO(result[0])
-    # return send_file(image, mimetype="image/png")
-    # for pythonanywhere
-    return Response(image, mimetype='image/jpeg', direct_passthrough=True)
+        if prm == "":
+            cur.execute(sql)
+        else:
+            cur.execute(sql, prm)
+        if fetchall:
+            data = cur.fetchall()
+        else:
+            data = cur.fetchone()
+        if commit:
+            con.commit()
+        else:
+            return data
 
-@app.route("/thumbnail/<int:id>", methods=["GET", "POST"])
-def thumbnail_route(id):
-    sql = "select thumbnail from msg where id = ?"
-    with sqlite3.connect(database) as con:
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute(sql, (id,))
-        result = cur.fetchone()
-    image = BytesIO(result[0])
+
+@app.route("/<kind>/<int:id>", methods=["GET", "POST"])
+def image_route(id, kind):
+    sql = f"select {kind} from msg where id = {id}"
+    # sql = "select ? from msg where id = ?"
+    data = db_crud(database=database, sql=sql, prm="",
+                   fetchall=False, commit=False)
+    image = BytesIO(data[0])
     # return send_file(image, mimetype="image/png")
     # for pythonanywhere
     return Response(image, mimetype='image/jpeg', direct_passthrough=True)
@@ -67,45 +65,37 @@ def msg():
     sql = {"read": "select * from msg order by id desc limit 99",
            "create": "insert into msg (name, msg, image, thumbnail) values (?, ?, ?, ?)"}
     if request.method == "GET":
-        with sqlite3.connect(database) as con:
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute(sql["read"])
-            data = cur.fetchall()
+        data = db_crud(database=database,
+                       sql=sql["read"], prm="", fetchall=True, commit=False)
         if data == []:
             return render_template("message.html", data="")
         return render_template("message.html", data=data)
     name = request.form['name']
     msg = request.form['msg']
     file = request.files["upload"]
-    file_exts = {'jpg', 'jpeg', "jfif", "png", "gif"}
-    image_size = (800, 800)
-    thumbnail_size = (300, 300)
     if not name:
         name = random.choice(["nobody", "anonymous", "路人甲", "無名"])
-    if check_file(file.filename, file_exts):
+    if check_file(file.filename):
+        image_size, thumbnail_size = (1000, 1000), (300, 300)
         buf_file, buf_thumb = BytesIO(), BytesIO()
-        # file.save(buf_file)
+        # file.save(buf_file) #just save
         im_image = Image.open(file)
         im_image.thumbnail(image_size)
         im_image.save(buf_file, get_file_ext(file.filename))
         image = sqlite3.Binary(buf_file.getbuffer())
+
         im_thumb = Image.open(file)
         im_thumb.thumbnail(thumbnail_size)
         im_thumb.save(buf_thumb, get_file_ext(file.filename))
-        # thumbnail = base64.b64encode(buf_thumb.getbuffer()).decode()
+        # thumbnail = base64.b64encode(buf_thumb.getbuffer()).decode() #base64
         thumbnail = sqlite3.Binary(buf_thumb.getbuffer())
-
     else:
         if msg:
             image, thumbnail = None, None
         else:
             return redirect(url_for("msg"))
-    with sqlite3.connect(database) as con:
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute(sql["create"], (name, msg, image, thumbnail))
-        con.commit()
+    db_crud(database=database, sql=sql["create"], prm=(
+        name, msg, image, thumbnail), fetchall=True, commit=True)
     return redirect(url_for("msg"))
 
 
