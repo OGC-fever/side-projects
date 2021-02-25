10c4790db5fd21233f1310d039ad3eac18be4dae
OGC-fever <seal.heart@gmail.com>
2021-02-25 23:38:31 +0800

    ajax msg

--- side project in flask/msg/static/msg_ajax.js
+++ side project in flask/msg/static/msg_ajax.js
@@ -14,7 +14,34 @@ $(document).ready(function () {
             dataType: "json",
             data: { "page": page },
             success: function (data) {
-                alert(data["id"], data["name"], data["msg"]);
+                // alert(data["id"].length);
+                // alert(data["name"]);
+                // alert(data["msg"]);
+                for (i = 0; i < data["id"].length; i++) {
+                    $('#ajax-insert').append(`
+                        <div class="col px-2 my-2">
+                            <div class="card h-100 bg-secondary info">
+                                <div class="cropped">
+                                    <img src="/timg/${id = data["id"][i]}" loading="lazy class="card-img img-fluid">
+                                </div>
+                                <a href="info/${id = data["id"][i]}">
+                                    <div class="card-img-overlay bg-dark px-2 py-2">
+                                        <div class="text-light px-1 pt-1 text-start">
+                                        ${data["msg"][i].replace("\n", "<br>")}
+                                        </div>
+                                        <div class="card-footer px-1 py-1 text-end">
+                                            <small class="blockquote-footer text-muted">
+                                            ${data["name"][i]}
+                                            </small>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>`
+                    );
+                }
+
+
             },
             error: function () {
                 alert("OOPS!");
