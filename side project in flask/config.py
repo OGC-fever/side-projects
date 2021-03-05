4c492d9603d5d56f63d9099cdc04857ff6f8a4e1
OGC-fever <seal.heart@gmail.com>
2021-03-06 00:11:49 +0800

    gcp test

--- side project in flask/config.py
+++ side project in flask/config.py
@@ -1,12 +1,12 @@
 from flask.app import Flask
 
-msg_app = Flask(__name__, static_folder="templates/msg/static")
+msg_app = Flask(__name__)
 msg_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apps/msg/message.db'
 msg_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 msg_app.config['JSON_AS_ASCII'] = False
 
 
-ptt_app = Flask(__name__, static_folder="templates/ptt/static")
+ptt_app = Flask(__name__)
 ptt_app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
 ptt_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ptt_app.config['JSON_AS_ASCII'] = False
