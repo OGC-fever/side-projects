d4faed8848b6ea40c3ece52888af5d53d3bb4db0
OGC-fever <seal.heart@gmail.com>
2021-03-10 00:08:08 +0800

    using ptt api

--- side project in flask/apps/ptt/ptt.py
+++ side project in flask/apps/ptt/ptt.py
@@ -3,35 +3,20 @@ from bs4 import BeautifulSoup as bs
 import requests
 from config import ptt_app
 from flask import render_template, request, redirect, url_for
+import math
 
 
-def get_back_url(url, page):
-    back_url = ""
-    for item in page.find_all(class_="btn wide"):
-        if "上頁" in (item.text):
-            back_url = url + "/" + item["href"].split("/")[-1]
-    return back_url
-
-
-def get_content(page):
+def get_content(html):
     data = []
-    for item in page.find_all("div", class_="r-ent"):
+    for item in html.find_all("div", class_="r-ent"):
         try:
             data.append([item.a.text,
-                         item.a["href"].split("/")[-1],
+                         item.a["href"].split("/", 2)[-1],
                          item.find(class_="date").text,
                          item.find(class_="author").text])
         except:
             pass
     return data
-
-
-def pure_data(data, keyword):
-    pure_data = []
-    for item in data:
-        if keyword.lower() in item[0].lower():
-            pure_data.append(item)
-    return pure_data
 
 
 @ptt_app.route("/", methods=["POST"])
@@ -41,64 +26,45 @@ def ptt_search():
     author = request.form['author']
     count = request.form['count']
     query_prms = ""
+    if not count:
+        count = 10
+    else:
+        count = int(count)
     if not board:
         board = "allpost"
-        if not keyword or not author:
-            return render_template("ptt/ptt.html")
     if keyword and author:
         query_prms = f"{keyword}+author:{author}"
     if keyword and not author:
-        query_prms = keyword
+        query_prms = f"{keyword}"
     if not keyword and author:
         query_prms = f"author:{author}"
-    url = f"https://www.ptt.cc/bbs/{board}/search?q={query_prms}"
+    if not keyword and not author:
+        return render_template("ptt/ptt.html")
+    url = "https://www.ptt.cc/bbs"
+    search_url = f"{url}/{board}/search?q={query_prms}"
     cookies = {'over18': "1"}
-    res = requests.get(url, cookies=cookies)
-    page = bs(res.content, 'lxml')
-    return str(page)
-    data = get_content(page)  # list
-    data.reverse()
-    data = pure_data(data, keyword)
-    while len(data) < count:
-        back_url = get_back_url(url, page)
-        res = requests.get(back_url, cookies=cookies)
-        page = bs(res.content, 'lxml')
-        temp = get_content(page)
-        temp.reverse()
-        data += temp
-        data = pure_data(data, keyword)
-
-# @ptt_app.route("/<board>/<keyword>/<int:count>", methods=["GET", "POST"])
-# def ptt(board, keyword, count):
-#     url = 'https://ptt.cc/bbs/' + board
-#     cookies = {'over18': "1"}
-#     res = requests.get(url, cookies=cookies)
-#     page = bs(res.content, 'lxml')
-#     data = get_content(page)  # list
-#     data.reverse()
-#     data = pure_data(data, keyword)
-#     while len(data) < count:
-#         back_url = get_back_url(url, page)
-#         res = requests.get(back_url, cookies=cookies)
-#         page = bs(res.content, 'lxml')
-#         temp = get_content(page)
-#         temp.reverse()
-#         data += temp
-#         data = pure_data(data, keyword)
-#     temp = 0
-#     json = {}
-#     for item in data:
-#         if len(json.keys()) >= count:
-#             break
-#         try:
-#             if keyword.lower() in item[0].lower():
-#                 temp += 1
-#                 json[str(temp).zfill(len(str(count)))] =\
-#                     {"title": item[0],
-#                      "url": url+"/"+item[1],
-#                      "date": item[2].strip(),
-#                      "author": item[3]}
-#         except TypeError:
-#             pass
-
-#     return render_template("ptt/ptt.html", data=json)
+    res = requests.get(search_url, cookies=cookies)
+    html = bs(res.content, 'lxml')
+    data = get_content(html)  # list
+    if len(data) > count:
+        data = data[:count]
+    elif len(data) < count:
+        page = math.ceil(count/20)
+        for page in range(2, page + 1):
+            back_url = f"{url}/{board}/search?page={page}&q={query_prms}"
+            res = requests.get(back_url, cookies=cookies)
+            html = bs(res.content, 'lxml')
+            temp = get_content(html)
+            data.extend(temp)
+    temp = 0
+    json = {}
+    for item in data:
+        if len(json.keys()) >= count:
+            break
+        temp += 1
+        json[str(temp).zfill(len(str(count)))] =\
+            {"title": item[0],
+                "url": f"{url}/{item[1]}",
+                "date": item[2].strip(),
+                "author": item[3]}
+    return render_template("ptt/ptt.html", data=json)
