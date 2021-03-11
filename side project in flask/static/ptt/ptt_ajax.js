c172273c98d776ada46ee06ba80d48e92127cfbb
OGC-fever <seal.heart@gmail.com>
2021-03-11 23:39:02 +0800

    pagination active

--- side project in flask/static/ptt/ptt_ajax.js
+++ side project in flask/static/ptt/ptt_ajax.js
@@ -1,17 +1,17 @@
 var page = 1;
 
-ajax_load(page);
+ajax_load(1);
 
 function ajax_board(json) {
     $('#ajax_board').html("");
     for (value in json) {
         if (value != "count") {
             $('#ajax_board').append(`
-            <tr>
-                <td>${value}</td>
-                <td>${json[value]["name"]}</td>
-                <td>${json[value]["title"]}</td>
-            </tr>`
+                <tr>
+                    <td>${value}</td>
+                    <td>${json[value]["name"]}</td>
+                    <td>${json[value]["title"]}</td>
+                </tr>`
             );
         }
     }
@@ -19,16 +19,26 @@ function ajax_board(json) {
 
 function ajax_pagination(json) {
     $('#ajax_pagination').html("");
-    for (i = 0; i < json["count"] / 10; i++) {
-        $('#ajax_pagination').append(`
-        <li class="page-item">
-            <a class="page-link" href="#" onclick="ajax_load(${i + 1});">${i + 1}</a>
-        </li>`
-        );
+    for (i = 1; i - 1 < json["count"] / 10; i++) {
+        if (i == page) {
+            $('#ajax_pagination').append(`
+                <li class="page-item active">
+                    <a class="page-link" href="#" onclick="ajax_load(${i});">${i}</a>
+                </li>`
+            );
+        } else {
+            $('#ajax_pagination').append(`
+                <li class="page-item">
+                    <a class="page-link" href="#" onclick="ajax_load(${i});">${i}</a>
+                </li>`
+            );
+        }
+
     }
 }
 
-function ajax_load(page) {
+function ajax_load(page_n) {
+    page = page_n;
     $('#ajax_list').show();
     $.ajax({
         url: "/ptt/list/" + page,
